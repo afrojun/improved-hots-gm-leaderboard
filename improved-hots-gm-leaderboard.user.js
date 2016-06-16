dfce72e312ca4f84bd71f3d930a5a171d8985cac
afrojun <afrojun@users.noreply.github.com>
2016-06-16 23:25:21 +0100

    Replace the Wins column with Win/Loss

--- improved-hots-gm-leaderboard.user.js
+++ improved-hots-gm-leaderboard.user.js
@@ -7,18 +7,19 @@
 // @grant       none
 // ==/UserScript==
 
-function addColumnHeader(tblClass, index, heading) {
-  var tblHeadObj = document.getElementsByClassName(tblClass)[0].tHead;
+var TABLE_CLASS = 'leaderboards-ranking-table';
+
+function addColumnHeader(index, heading) {
+  var tblHeadObj = document.getElementsByClassName(TABLE_CLASS)[0].tHead;
   var newTH = tblHeadObj.rows[0].insertCell(index);
   newTH.outerHTML = '<th>' + heading + '</th>';
 }
 
 function addWinPercentColumn()
 {
-  var tblClass = 'leaderboards-ranking-table';
-  addColumnHeader(tblClass, 5, 'WIN %')
+  addColumnHeader(5, 'WIN %')
 
-  var tblBodyObj = document.getElementsByClassName(tblClass)[0].tBodies[0];
+  var tblBodyObj = document.getElementsByClassName(TABLE_CLASS)[0].tBodies[0];
   for (var i=0; i<tblBodyObj.rows.length; i++) {
     var newCell = tblBodyObj.rows[i].insertCell(5);
     var tblRow = tblBodyObj.rows[i];
@@ -28,9 +29,7 @@ function addWinPercentColumn()
 
 function addLinkToHotsLogs()
 {
-  var tblClass = 'leaderboards-ranking-table';
-
-  var tblBodyObj = document.getElementsByClassName(tblClass)[0].tBodies[0];
+  var tblBodyObj = document.getElementsByClassName(TABLE_CLASS)[0].tBodies[0];
   for (var i=0; i<tblBodyObj.rows.length; i++) {
     var battleTagCell = tblBodyObj.rows[i].cells[2];
 
@@ -44,5 +43,19 @@ function addLinkToHotsLogs()
   }
 }
 
+function addWinLossColumn() {
+  var winHeader = document.getElementsByClassName(TABLE_CLASS)[0].tHead.rows[0].cells[4];
+  winHeader.innerHTML = 'WIN/LOSS';
+
+  var tblBodyObj = document.getElementsByClassName(TABLE_CLASS)[0].tBodies[0];
+  for (var i=0; i<tblBodyObj.rows.length; i++) {
+    var tblRow = tblBodyObj.rows[i];
+    var wins = tblRow.cells[4].innerHTML;
+    var losses = parseInt(tblRow.cells[3].innerHTML) - parseInt(tblRow.cells[4].innerHTML);
+    tblRow.cells[4].innerHTML = wins + '/' + losses;
+  }
+}
+
 addWinPercentColumn();
 addLinkToHotsLogs();
+addWinLossColumn();
